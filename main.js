0b497f0f09689c4a95a53f9862d5f4acdb6d7dd7
trfink19 <47217858+trfink19@users.noreply.github.com>
2019-05-16 14:33:03 -0400

    Rooms and Items - Tristan Fink

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
-
 // Create the contents of your room here.
 alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
 //Create your objects
-let bedroom1 = new Room("Bedroom1","This room is very dark and has many items including a painting, lamp, bed, vase, and a desk.")
-let bedroom2 = new Room("Bedroom2","Oh my there is a person on the bed and he looks dead.")
+let bedroom1 = new Room("Bedroom1","This room is very dark and has many items including a painting, lamp, bed, vase, closet, and a desk.")
+let bedroom2 = new Room("Bedroom2","Oh my there is a person on the bed and he looks dead and a closet in the corner.")
 let kitchen = new Room("kitchen","This kitchen is completely empty but something is in the corner.")
 let basement = new Room("basement","The only thing in the basement are chains and bones that are laying on the floor.")
 let bathroom = new Room("bathroom","There is a bloody knife next to the sink and the bathtub is filled with blood.")
@@ -18,6 +16,9 @@ let chains = new Item("chains",".")
 let bones = new Item("bones",".")
 let knife = new Item("knife",".")
 let sink = new Item("sink",".")
+let closet1 = new Item("closet1",".")
+let closet2 = new Item("closet2",".")
+
 
 
 
@@ -25,8 +26,12 @@ let sink = new Item("sink",".")
 bedroom1.addItem(lamp);
 bedroom1.addItem(painting);
 bedroom1.addItem(vase);
+bedroom1.addItem(closet1)
 bedroom2.addItem(dead body);
+bedroom2.addItem(closet2);
 basement.addItem(chains);
 basement.addItem(bones);
 bathroom.addItem(sink);
 bathroom.addItem(knife);
+
+let
