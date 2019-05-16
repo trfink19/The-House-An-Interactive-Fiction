dacb8edb969a347a1547c277db1627212c1956bd
staplejm <staplejm@dukes.jmu.edu>
2019-05-16 13:48:36 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
-console.log("Loading game");
 //Create your objects
 let hallway = new Room("dusty hallway", "clouds of dust kick up with every step.");
 let vase = new Item("vase", "made of blue glass, chipped on top. Filled with a dark liquid.")
