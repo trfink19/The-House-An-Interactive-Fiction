ca9ad65d1e1badb1fc17a7c29d43649e624491d4
trfink19 <47217858+trfink19@users.noreply.github.com>
2019-05-17 14:34:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,35 +3,45 @@ alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
 //Create your objects
-let bedroom1 = new Room("Bedroom1","This room is very dark and has many items including a painting, lamp, bed, vase, closet, and a desk.")
-let bedroom2 = new Room("Bedroom2","Oh my there is a person on the bed and he looks dead and a closet in the corner.")
+let hallway = new Room("hallway","This hallway is very creepy.")
+let bedroom = new Room("bedroom","This room is very dark and has many items including a painting, lamp, bed, vase, closet, and a desk.")
+let masterBedroom = new Room("Master bedroom","Oh my there is a person on the bed and he looks dead and a closet in the corner.")
 let kitchen = new Room("kitchen","This kitchen is completely empty but something is in the corner.")
 let basement = new Room("basement","The only thing in the basement are chains and bones that are laying on the floor.")
 let bathroom = new Room("bathroom","There is a bloody knife next to the sink and the bathtub is filled with blood.")
 let lamp = new Item("lamp",".")
 let painting = new Item("painting",".")
 let vase = new Item("vase",".")
-let deadBody = new Item("deadBody",".")
+let deadBody = new Item("dead body",".")
 let chains = new Item("chains",".")
 let bones = new Item("bones",".")
 let knife = new Item("knife",".")
 let sink = new Item("sink",".")
-let closet1 = new Item("closet1",".")
-let closet2 = new Item("closet2",".")
+let closet = new Item("closet",".")
+let Closet = new Item("closet",".")
+let darkFigure = new Item("dark figure",".")
+let bloodyBathtub = new Item("bloody bathtub",".")
 
 
 
 
 // Put them in their spots
-bedroom1.addItem(lamp);
-bedroom1.addItem(painting);
-bedroom1.addItem(vase);
-bedroom1.addItem(closet1)
-bedroom2.addItem(dead body);
-bedroom2.addItem(closet2);
+bedroom.addItem(lamp);
+bedroom.addItem(painting);
+bedroom.addItem(vase);
+bedroom.addItem(closet)
+masterBedroom.addItem(deadBody);
+masterBedroom.addItem(Closet);
 basement.addItem(chains);
 basement.addItem(bones);
 bathroom.addItem(sink);
 bathroom.addItem(knife);
+bathroom.addItem(bloodyBathtub)
+kitchen.addItem(darkFigure)
+hallway.addItem(masterBedroom);
+hallway.addItem(bedroom);
+hallway.addItem(bathroom);
+hallway.addItem(kitchen);
+hallway.addItem(basement);
 
-let
+player.move(hallway);
