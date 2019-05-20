a538f823d16b88b817c46921729e24ae9c9c4349
staplejm <staplejm@dukes.jmu.edu>
2019-05-20 07:46:55 -0400

    Create jon.js

---
+++ scripts/jon.js
@@ -0,0 +1,8 @@
+alert("Loading Jon's module!);
+
+player = new Player();
+
+let livingRoom = new Room('living room', 'It is a living room. The floor is carpeted.');
+//add more rooms and items here...
+
+player.move(livingRoom);
