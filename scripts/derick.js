ea1af972868f52eb347e25fe54452b7acf95891d
staplejm <staplejm@dukes.jmu.edu>
2019-05-20 09:50:33 -0400

    Add files via upload

--- scripts/derick.js
+++ scripts/derick.js
@@ -1,40 +1,39 @@
 
-
 // Create the contents of your room here.
 alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
 //Create your objects
-let aRoom = new Room("aRoom","A cold room with only a table and a tv with a message and a note");
-let tv = new Item("tv", "with a message on the screen-- You must find all the missing items that are around the diferent rooms in this section of the house");
-let note = new Item("note","A note right next to the tv with a message that reads--You are in for a surprise")
-let livingRoom = new Room("livingRoom","A samll livingRoom with two tables and a sofa");
-let thumbDrive = new Item("thumbDrive","Siting on the big table a thumbDrive holds something withing it");
-let battery = new Item("bettery","A very shiny bettery and a powerfull one as well it can last for two months.");
-let library = new Room("library"," A small sized library with only two book stands");
-let headset = new Item("headset","A nice lookign headset with a gold plating the sound quality is amazing.");
-let basement = new Room("basement","It feels very cold but there is sometihng legendary here.");
-let legenDaryController = new Item("legenDary Controller","Inside the chest there is a legendary gaming controlle it is plated with pure gold.")
-let chest = new Item("chest","Inside of it there is sometihng trully special.");
-let TheRoom = new Room("TheRoom","TheRom has a big tv and a very comfortable chair.");
-let bigTV = new Item("bigTV"," a 4k gaming TV.");
-let TvRemote = new Item("Tv Remote","A silver plated remote for the tv.");
-let chair = new Item("chair"," very comfortable chair with a soda dispenser.");
-
-TheRoom.addItem(bigTV);
-TheRoom.addItem(TvRemote);
-TheRoom.addItem(chair);
-basement.addItem(legenDaryController);
-basement.addItem(chest);
-livingRoom.addItem(thumbDrive);
-livingRoom.addItem(battery);
-library.addItem(headset);
-aRoom.addItem(TheRoom);
-aRoom.addItem(basement);
-aRoom.addItem(tv);
-aRoom.addItem(livingRoom);
-aRoom.addItem(library);
-aRoom.addItem(note);
+let foyer = new Room('foyer', 'There is a large staircase, and the floor is made of smooth concrete.');
+let chandelier = new Item('chandelier', 'above you, made of glass and steel.');
+let attic = new Room('attic', 'dark, but you see a glint of light.');
+let lantern = new Item('lantern', 'A source of light, but you need a candle to have more light.');
+let gps = new Item('gps', 'there is also gps that leads to somewhere.')
+let bedroom = new Room('bedroom', 'The room is a still all in one piece, and not ruined at all.');
+let candle = new Item('candle', 'There is a candle that can be used for your lantern.');
+let key = new Item('key', 'There is also a key that is used to unlock something.');
+let backyard = new Room('backyard', 'Its a big field and you are gonna be walking for quite a wild.');
+let cutters = new Item('cutters', 'you walk around and find a pair of cutters.');
+let lock = new Item('lock', 'Its a lock, but there is no keyhole on it.');
+let storage = new Room('storage', ' While walking you see what looks like a storage.');
+let toolbox = new Item('toolbox', 'The storage dusty and with old stuff, but you find a toolbox.');
+let wrench = new Item('wrench', 'the toolbox is filled many items and one of them is a wrench');
+let trowel = new Item('trowel', 'There seems to be a rusty trowel as well.');
 
 
-player.move(aRoom);
+foyer.addItem(chandelier);
+foyer.addItem(attic);
+attic.addItem(lantern);
+attic.addItem(gps);
+foyer.addItem(bedroom);
+bedroom.addItem(candle);
+bedroom.addItem(key);
+foyer.addItem(backyard);
+backyard.addItem(cutters);
+backyard.addItem(lock);
+backyard.addItem(storage);
+storage.addItem(toolbox);
+storage.addItem(wrench);
+storage.addItem(trowel);
+
+player.move(foyer);
